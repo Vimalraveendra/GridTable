7b336ae10be38f0d761d9f502bdd5b9c3bf0c728
Vimal Raveendran <vimalraveendra@gmail.com>
2021-07-21 22:40:15 +0200

    updated indexRazor component to dynamically render the  table data

---
+++ GridTable/wwwroot/Script.js
@@ -0,0 +1,46 @@
+﻿
+window.logUser = window.logUser ||
+    function () {
+        loadUsers();
+    }
+
+
+function loadUsers() {
+
+
+let personData = [
+    {
+    Name:"John"
+    },
+    {
+        Name: "Boss"
+    },
+    {
+        Name: "Denver"
+    },
+    {
+        Name: "Pawel"
+    },
+    {
+        Name: "Xavier"
+    },
+]
+
+
+
+  
+    loadTableData(personData)
+
+function loadTableData(personData) {
+    console.log("ehheeh")
+    let tableBody = document.getElementById('table-data')
+    let dataHtml = '';
+
+    for (person of personData) {
+        dataHtml += `<tr><td></td><td>${person.Name}</td><td></td></tr>`
+    }
+    tableBody.innerHTML = dataHtml;
+
+}
+
+}
