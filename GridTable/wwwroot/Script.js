46c1a2e5e6a776fa104adfc57cc3d7a89b98690e
Vimal Raveendran <vimalraveendra@gmail.com>
2021-07-22 22:05:10 +0200

    updated IndexRazor component & Script files to dynamically render the table with active class

--- GridTable/wwwroot/Script.js
+++ GridTable/wwwroot/Script.js
@@ -6,41 +6,95 @@ window.logUser = window.logUser ||
 
 
 function loadUsers() {
+    const inputValueEl = document.getElementById('name')
+
+    //let personData = [
+    //    {
+    //        Name: "John"
+    //    },
+    //    {
+    //        Name: "Boss"
+    //    },
+    //    {
+    //        Name: "Denver"
+    //    },
+    //    {
+    //        Name: "Pawel"
+    //    },
+    //    {
+    //        Name: "Xavier"
+    //    },
+    //]
 
 
-let personData = [
-    {
-    Name:"John"
-    },
-    {
-        Name: "Boss"
-    },
-    {
-        Name: "Denver"
-    },
-    {
-        Name: "Pawel"
-    },
-    {
-        Name: "Xavier"
-    },
-]
+
+
+    //loadTableData(personData)
+
+    //function loadTableData(personData) {
+       
+    //    let tableBody = document.getElementById('table-data')
+    //    let dataHtml = '';
+    //    var current = document.getElementsByClassName("active");
+    //    for (person of personData) {
+    //        dataHtml += `<tr><td></td><td>${person.Name}</td><td></td></tr>`
+    //    }
+    //    // If there's no active class
+    //    if (current.length > 0) {
+    //        current[0].className = current[0].className.replace(" active", "");
+    //    }
+
+    //    tableBody.innerHTML = dataHtml;
 
 
 
-  
-    loadTableData(personData)
+    
 
-function loadTableData(personData) {
-    console.log("ehheeh")
-    let tableBody = document.getElementById('table-data')
-    let dataHtml = '';
+    //    const tRowContainerEl = document.querySelectorAll('.table-data tr')
 
-    for (person of personData) {
-        dataHtml += `<tr><td></td><td>${person.Name}</td><td></td></tr>`
-    }
-    tableBody.innerHTML = dataHtml;
 
-}
+    
 
+
+    //}
+
+    let count = 1;
+
+    window.setInputTextToTable = () => {
+        text =inputValueEl.value
+        if (text) {
+            let tableBody = document.getElementById('table-data')
+            var current = document.getElementsByClassName("active");
+            console.log('cur', current)
+
+            if (current.length > 0) {
+                current[0].className = current[0].className.replace(" active", "");
+            }
+
+            
+            tableBody.innerHTML += `<tr><td >${count}</td><td class=" active">${text}</td><td></td></tr>`
+
+            inputValueEl.value = "";
+        }
+        count++;
+
+        const tRowContainerEl = document.querySelectorAll('.table-data tr')
+        console.log("table", tRowContainerEl)
+        tRowContainerEl.forEach(list => {
+            list.addEventListener("click", function () {
+                var current = document.getElementsByClassName("active");
+                console.log('cur', current)
+
+                if (current.length > 0) {
+                    current[0].className = current[0].className.replace(" active", "");
+                }
+
+                list.childNodes[1].className += " active";
+
+            })
+        })
+    };
+
+
+    
 }
